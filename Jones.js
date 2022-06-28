6cf999a12ce09159fb2e73b74d3a3552f5c425f9
LOCO185 <jloco151@gmail.com>
2022-06-28 11:59:44 +0300

    ..

--- Jones.js
+++ Jones.js
@@ -21,13 +21,16 @@ async function example() {
 
   //  Create a screenshot of the page before clicking the “Request a call back” button.
   await page.screenshot({ path: "Page1.png" });
-  
+
   // Click the “Request a call back” button
   await Promise.all([
     await page.click(
       "body > div > div.row > div.large-5.medium-5.columns > div > form > p:nth-child(8) > button"
     ),
   ]);
+
+  // Screenshot Thank You page
+  await page.screenshot({ path: "Page2.png" });
 
   await browser.close();
 }
