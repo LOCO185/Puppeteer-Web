95938a594861e59a5b5ff5748737dbcc3321247f
LOCO185 <jloco151@gmail.com>
2022-06-29 16:30:55 +0300

    ..

--- Jones.js
+++ Jones.js
@@ -2,13 +2,4 @@ const puppeteer = require("puppeteer");
 
 const URL = "http://contractorsinsurancereview.com/ExampleForm/";
 
-async function example() {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(URL);
-
-
-  await browser.close();
-}
-
 example();
